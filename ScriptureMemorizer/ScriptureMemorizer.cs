9dce37b46c1cc75e583837fcdfb7e38a55d95deb
levenbarney <113639602+levenbarney@users.noreply.github.com>
2023-02-18 10:32:25 -0700

    new repo for Scripture Memorizer

---
+++ ScriptureMemorizer/ScriptureMemorizer.cs
@@ -0,0 +1,53 @@
+using System;
+
+class ScriptureMemorizer
+{
+    private Scripture scripture;
+    private List<string> scriptureTextList;
+    
+    public ScriptureMemorizer(Scripture _scripture)
+    {
+        scripture = _scripture;
+        scriptureTextList = new List<string>();
+        convertTextToList();
+    }
+
+    private void convertTextToList()
+    {
+        scriptureTextList = scripture.toString().Split(' ').ToList();
+    }
+
+    public void removeWordsFromText()
+    {
+        int numWordsToRemove = new Random().Next(2,4);
+        int wordsRemoved = 0;
+
+            do
+            {
+                int rndIndex= new Random().Next(0, scriptureTextList.Count());
+                scriptureTextList[rndIndex] = new string('_', scriptureTextList[rndIndex].Length);
+                wordsRemoved++;
+
+            }while (wordsRemoved!= numWordsToRemove);
+    }
+
+    public string ToString()
+    {
+        return string.Join(' ', scriptureTextList);
+    }
+
+    public bool hasWordsLeft()
+    {
+        bool retvalue = false;
+
+        foreach (string word in scriptureTextList)
+        {
+            if (word.Contains('_') == false)
+            {
+                retvalue - true;
+                break;
+            }
+        }
+        return retvalue;
+    }
+}
